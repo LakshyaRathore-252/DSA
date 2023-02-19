78e24f6df982b5eff250e3b8e4d839f69cee3a77
Lakshya Rathore <97387362+LakshyaRathore-252@users.noreply.github.com>
2023-02-19 18:47:38 +0530

    Update 287. Find the Duplicate Number.cpp

--- 287. Find the Duplicate Number.cpp
+++ 287. Find the Duplicate Number.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        
+        // Using Sorting Technique
         int ans;
-        sort(nums.begin() , nums.end());
+        sort(nums.begin() , nums.end()); //Time Complexity for sorting is o(nlogn) and Space Complexity is O(n).
         for(int i = 0 ; i < nums.size()-1; i++)
         {
             if(nums[i] == nums[i+1])
@@ -12,6 +14,35 @@ public:
         }
 
         return ans;
+        
+    }
+};
+
+// ======================================================== Negative Marking Methode ==================================================================
+
+// Visited Solution
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int ans = -1;
+        
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+           int index = abs(nums[i]);
+
+            // Already Visited
+           if(nums[index] < 0)
+           {
+               ans = index;
+               break;
+           }
+
+            // Visited Mark
+           nums[index] *= -1;
+        }
+
+      return ans;
         
     }
 };
